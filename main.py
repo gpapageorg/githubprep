0d6d99efd860e05edd25c436d7a36716d403b105
george <georg.papageorg@gmail.com>
2024-01-07 01:07:34 +0200

    Added Help

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+'''
+#####################################################################
+                _____ _____ _______ _    _ _    _ ____
+               / ____|_   _|__   __| |  | | |  | |  _ \
+              | |  __  | |    | |  | |__| | |  | | |_) |
+              | | |_ | | |    | |  |  __  | |  | |  _ <
+              | |__| |_| |_   | |  | |  | | |__| | |_) |
+               \_____|_____|  |_|  |_|  |_|\____/|____/
+  _____  _____  ______ _____        _____        _______ ____  _____
+ |  __ \|  __ \|  ____|  __ \ /\   |  __ \    /\|__   __/ __ \|  __ \
+ | |__) | |__) | |__  | |__) /  \  | |__) |  /  \  | | | |  | | |__) |
+ |  ___/|  _  /|  __| |  ___/ /\ \ |  _  /  / /\ \ | | | |  | |  _  /
+ | |    | | \ \| |____| |  / ____ \| | \ \ / ____ \| | | |__| | | \ \
+ |_|    |_|  \_|______|_| /_/    \_|_|  \_/_/    \_|_|  \____/|_|  \_\
+
+#####################################################################
+'''
+
+
+
 from github import Github
 from github import Auth
 import sys, os
 
 TOKEN_FILENAME = "token"
-COMMANDS = ["createRepo","deleteRepo", "defaultRoutine", "listRepos"]
+COMMANDS = ["createRepo","deleteRepo", "defaultRoutine", "listRepos", "help"]
 MAXARGS = 3 ### MAXIMUM ARGUMENT NUMBER ###
 
 argc = len(sys.argv)
@@ -54,8 +74,8 @@ def initializeGit():
 
 
 def checkArgs():
-    if argc > MAXARGS:
-        print("Wrong Argument Number")
+    if argc > MAXARGS or argc == 1:
+        print('''Wrong Argument Number use "help" to get help.''')
         exit()
 
     if argv[0] not in COMMANDS:
@@ -100,7 +120,14 @@ def executeCommand(i):
         except:
             print("Error While Listing Repos")
             exit()
-
+    elif argv[0] == COMMANDS[4] and argc == 2:
+        print('''GitHub Repository Preperator, This Tool Prepares The Enviroment And The GitHub Repository.
+        This Tool Has The Following Options
+        1) Default Routine, which initisializes git and writes the first README.md file, then it creates a repository with
+            the given name, it also creates 2 branches one main (main) and one for development (developMain), and pushes the
+            changes to the repository  | use (defaultRoutine "NAME OF REPOSITORY")
+        2) Create Repository To Create A New Repository | use (CreateRepo "NAME OF REPOSITORY")
+        3) Delete Repository | use (deleteRepo "NAME OF REPOSITORY")''')
     else:
         print("Error In Executing Command")
         exit()
